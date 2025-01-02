c654ceb1d16569b7b88c5ed71ccf7753f5ba811f
Shruti <03shruticl@gmail.com>
2025-01-02 17:14:00 +0530

    update

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,18 @@
 import { cart, removeFromCart } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
+import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+// import formatCurrency from './utils/money.js';
+
+hello();
+
+const today = dayjs();
+const deliveryDate = today.add(7, 'days');
+console.log(
+  deliveryDate.format('dddd, MMMM D'));
+
+
 
 let cartSummaryHTML = '';
 
