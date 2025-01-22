2ac19c95d74fb6d653c8dea0210c1536eef610c5
Shruthi L <Shruthi@agpl.net>
2025-01-22 15:58:26 +0530

    update

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,5 +1,5 @@
 import { cart, removeFromCart, updateDeliveryOption } from '../../data/cart.js';
-import { products,getProduct } from '../../data/products.js';
+import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
@@ -19,7 +19,7 @@ export function renderOrderSummary() {
 
     const deliveryOptionId = cartItem.deliveryOptionId;
 
-    const deliveryOption =getDeliveryOption(deliveryOptionId);
+    const deliveryOption = getDeliveryOption(deliveryOptionId);
 
     const today = dayjs();
     const deliveryDate = today.add(
@@ -31,7 +31,7 @@ export function renderOrderSummary() {
     );
 
     cartSummaryHTML += `
-      <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+      <div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: ${dateString}
             </div>
@@ -46,14 +46,15 @@ export function renderOrderSummary() {
                 <div class="product-price">
                   ₹${formatCurrency(matchingProduct.priceCents)}
                 </div>
-                <div class="product-quantity">
+                <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                   <span>
                     Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                   </span>
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+                  <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" 
+                    data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
