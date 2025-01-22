1dc28453ec0b494bc3eeb531a963889e61c8e437
Shruthi L <Shruthi@agpl.net>
2025-01-22 11:57:04 +0530

    Create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,22 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if (!cart) {
-    cart = [{
-        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 2,
-        deliveryOptionId: '1'
-    }, {
-        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity: 1,
-        deliveryOptionId: '2'
-    }];
+loadFromStorage();
+
+export function loadFromStorage() {
+    cart = JSON.parse(localStorage.getItem('cart'));
+
+    if (!cart) {
+        cart = [{
+            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity: 2,
+            deliveryOptionId: '1'
+        }, {
+            productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+            quantity: 1,
+            deliveryOptionId: '2'
+        }];
+    }
+
 }
 
 function saveToStorage() {
@@ -48,7 +55,7 @@ export function removeFromCart(productId) {
     saveToStorage();
 }
 
- export function updateDeliveryOption(productId, deliveryOptionId){
+export function updateDeliveryOption(productId, deliveryOptionId) {
     let matchingItem;
 
     cart.forEach((cartItem) => {
@@ -57,7 +64,7 @@ export function removeFromCart(productId) {
         }
     });
 
-    matchingItem.deliveryOptionId=deliveryOptionId;
+    matchingItem.deliveryOptionId = deliveryOptionId;
 
     saveToStorage();
 
