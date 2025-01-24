2f0d228eda900bc10d6ea8ac36e039e44c993881
Shruthi L <Shruthi@agpl.net>
2025-01-24 15:42:23 +0530

    Create oop version of cart

--- data/cart-oop.js
+++ data/cart-oop.js
@@ -1,78 +1,90 @@
-const cart = {
-    cartItems: undefined,
+function Cart(localStorageKey) {
+    const cart = {
+        cartItems: undefined,
 
-    loadFromStorage: function () {
-        cart = JSON.parse(localStorage.getItem('cart'));
+        loadFromStorage() {
+            this.cartItems = JSON.parse(localStorage.getItem(localStorageKey));
 
-        if (!cart) {
-            cart = [{
-                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-                quantity: 2,
-                deliveryOptionId: '1'
-            }, {
-                productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-                quantity: 1,
-                deliveryOptionId: '2'
-            }];
+            if (!this.cartItems) {
+                this.cartItems = [{
+                    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                    quantity: 2,
+                    deliveryOptionId: '1'
+                }, {
+                    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+                    quantity: 1,
+                    deliveryOptionId: '2'
+                }];
+            }
+
+        },
+
+        saveToStorage() {
+            localStorage.setItem(localStorageKey, JSON.stringify(this.cartItems));
+        },
+
+
+        addToCart(productId) {
+            let matchingItem;
+
+            this.cartItems.forEach((cartItem) => {
+                if (productId === cartItem.productId) {
+                    matchingItem = cartItem;
+                }
+            });
+
+            if (matchingItem) {
+                matchingItem.quantity += 1;
+            } else {
+                this.cartItems.push({
+                    productId: productId,
+                    quantity: 1,
+                    deliveryOptionId: '1'
+                });
+            }
+            this.saveToStorage();
+        },
+
+        removeFromCart(productId) {
+            const newCart = [];
+            this.cartItems.forEach((cartItem) => {
+                if (cartItem.productId !== productId) {
+                    newCart.push(cartItem);
+                }
+            });
+            this.cartItems = newCart;
+            this.saveToStorage();
+        },
+
+
+
+        updateDeliveryOption(productId, deliveryOptionId) {
+            let matchingItem;
+
+            this.cartItems.forEach((cartItem) => {
+                if (productId === cartItem.productId) {
+                    matchingItem = cartItem;
+                }
+            });
+
+            matchingItem.deliveryOptionId = deliveryOptionId;
+
+            this.saveToStorage();
+
         }
+    };
+    return cart;
 
-    }
-
-};
-
-
-
-loadFromStorage();
-
-
-
-function saveToStorage() {
-    localStorage.setItem('cart', JSON.stringify(cart));
 }
 
-export function addToCart(productId) {
-    let matchingItem;
+const cart = Cart('cart-oop');
+const businessCart = Cart('cart-business');
 
-    cart.forEach((cartItem) => {
-        if (productId === cartItem.productId) {
-            matchingItem = cartItem;
-        }
-    });
 
-    if (matchingItem) {
-        matchingItem.quantity += 1;
-    } else {
-        cart.push({
-            productId: productId,
-            quantity: 1,
-            deliveryOptionId: '1'
-        });
-    }
-    saveToStorage();
-}
+cart.loadFromStorage();
 
-export function removeFromCart(productId) {
-    const newCart = [];
-    cart.forEach((cartItem) => {
-        if (cartItem.productId !== productId) {
-            newCart.push(cartItem);
-        }
-    });
-    cart = newCart;
-    saveToStorage();
-}
 
-export function updateDeliveryOption(productId, deliveryOptionId) {
-    let matchingItem;
+businessCart.loadFromStorage();
 
-    cart.forEach((cartItem) => {
-        if (productId === cartItem.productId) {
-            matchingItem = cartItem;
-        }
-    });
-
-    matchingItem.deliveryOptionId = deliveryOptionId;
-
-    saveToStorage();
-
-}
+console.log(cart);
+console.log(businessCart);
