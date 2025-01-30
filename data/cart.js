389e8c983c2918bf7eac75fc12d265dc4199d44a
Shruthi L <Shruthi@agpl.net>
2025-01-30 12:59:43 +0530

    Learn promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -67,5 +67,20 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
     matchingItem.deliveryOptionId = deliveryOptionId;
 
     saveToStorage();
+}
 
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    console.log(xhr.response);
+    
+
+    // console.log('load products');
+    fun();
+
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
 }
